5e082b4dc353933a8992286283bb48e1f19afde8
shamoto <thebicameralmind9@gmail.com>
2019-03-18 12:08:49 +0000

    twitterログイン時メールアドレスをダミーで登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,20 +15,39 @@ class User < ApplicationRecord
     SecureRandom.uuid
   end
 
-
-  def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
-    user = User.find_by(provider: auth.provider, uid: auth.uid)
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
 
     unless user
-      user = User.find_by(provider: auth.provider,
-                      uid:      auth.uid,
-                      email:    "#{auth.uid}-#{auth.provider}@example.com",
-                      name: auth.info.name,
-                      avatar: auth.info.avatar,
-                      password: Devise.friendly_token[0, 20]
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20],
+        name: auth.info.name,
+        username: auth.info.nickname
       )
     end
-    user.save
     user
+  end
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
+
+　private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
   end
 end
